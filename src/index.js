2b90851f45e41b085a776b6041559cc47cf53247
Jonathan <jrugama96@gmail.com>
2021-04-28 12:53:24 -0600

    Se borra lo no utilizado

--- src/index.js
+++ src/index.js
@@ -7,8 +7,6 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import BadgeNew from './pages/BadgeNew';
-import Badges from './pages/Badges';
 import 'bootstrap/dist/css/bootstrap.css';
 import './global.css'
 import App from './components/App'
